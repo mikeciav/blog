9ffb22a6af2e9b97bd2f7ab6092b9691d91e23d2
Mike Ciavarella <mciav5@gmail.com>
2017-01-29 14:21:23 -0500

    fixing post layout

--- resources/views/b/s.blade.php
+++ resources/views/b/s.blade.php
@@ -2,77 +2,78 @@
 
 @section('content')
 	<div class="row">
-		<div class="col-md-12 well">
-			<h2>{{$post->title}}</h2>
-			<hr>
-				<span>
-					<i class='glyphicon glyphicon-user' aria-hidden='true'></i> 
-					Mike "Ragamuffin" Ciavarella
-					<span class='pull-right'>
-						<i class='glyphicon glyphicon-time' aria-hidden='true'></i>
-						<small>{{$post->created_at->format('F jS Y g:i A')}}</small>
+		<div class="col-md-12">
+			<div class="col-md-12 well">
+				<h2>{{$post->title}}</h2>
+				<hr>
+					<span>
+						<i class='glyphicon glyphicon-user' aria-hidden='true'></i> 
+						Mike "Ragamuffin" Ciavarella
+						<span class='pull-right'>
+							<i class='glyphicon glyphicon-time' aria-hidden='true'></i>
+							<small>{{$post->created_at->format('F jS Y g:i A')}}</small>
+						</span>
 					</span>
-				</span>
-			<hr>
-			<img src="{{asset('photos/'.$post->image)}}" alt="" class='img-responsive img-headline'>
-			<hr>
-			<p class='lead'><i>{{$post->tagline}}</i></p>
-			<hr>
-			{!!$post->body!!}
-			<hr>
-			<p class="footer"><small>{!!$post->footer!!}</small></p>
-			<hr>
-			<div class="row">
-				@if(Auth::check())
-					<div class='col-md-3'>
-						@if($is_fav = in_array($post->id, $fav))
-							<form action="{{route('favorites.destroy', [Auth::id(), $post->id])}}" method='post'>
-								{{method_field('DELETE')}}{{csrf_field()}}
-								<button type='submit' class='btn-fav'>
-									<i class='fa fa-2x fa-heart is-fav' aria-hidden='true' title='Remove from favorites'></i>
-								</button>
-							</form>
-						@else
-							<form action="{{route('favorites.store', $post->id)}}" method='post'>
-								{{csrf_field()}}
-								<button type='submit' class='btn-fav'>
-									<i class='fa fa-2x fa-heart-o not-fav' aria-hidden='true' title='Add to favorites'></i>
-								</button>
-							</form>
+				<hr>
+				<img src="{{asset('photos/'.$post->image)}}" alt="" class='img-responsive img-headline'>
+				<hr>
+				<p class='lead'><i>{{$post->tagline}}</i></p>
+				<hr>
+				{!!$post->body!!}
+				<hr>
+				<p class="footer"><small>{!!$post->footer!!}</small></p>
+				<hr>
+				<div class="row">
+					@if(Auth::check())
+						<div class='col-md-3'>
+							@if($is_fav = in_array($post->id, $fav))
+								<form action="{{route('favorites.destroy', [Auth::id(), $post->id])}}" method='post'>
+									{{method_field('DELETE')}}{{csrf_field()}}
+									<button type='submit' class='btn-fav'>
+										<i class='fa fa-2x fa-heart is-fav' aria-hidden='true' title='Remove from favorites'></i>
+									</button>
+								</form>
+							@else
+								<form action="{{route('favorites.store', $post->id)}}" method='post'>
+									{{csrf_field()}}
+									<button type='submit' class='btn-fav'>
+										<i class='fa fa-2x fa-heart-o not-fav' aria-hidden='true' title='Add to favorites'></i>
+									</button>
+								</form>
+							@endif
+						</div>					
 						@endif
-					</div>					
-					@endif
-					<div class='col-md-5'>
-						<span class='st_facebook_large'></span>
-						<span class='st_twitter_large'></span>
-						<span class='st_reddit_large'></span>
-						<span class='st_googleplus_large'></span>
-						<span class='st_wordpress_large'></span>
-						<span class='st_blogger_large'></span>
+						<div class='col-md-5'>
+							<span class='st_facebook_large'></span>
+							<span class='st_twitter_large'></span>
+							<span class='st_reddit_large'></span>
+							<span class='st_googleplus_large'></span>
+							<span class='st_wordpress_large'></span>
+							<span class='st_blogger_large'></span>
+						</div>
+					<div class="col-sm-4 pull-right">
+						<a href="{{url('/')}}" class='btn btn-default btn-block'>Return to Posts</a>
 					</div>
-				<div class="col-sm-4 pull-right">
-					<a href="{{url('/')}}" class='btn btn-default btn-block'>Return to Posts</a>
 				</div>
+				<hr>
+				@include('partials.disqus')
 			</div>
-			<hr>
-			@include('partials.disqus')
-		</div>
-		@if(Auth::user() && Auth::user()->isAdmin())
-			<hr>
-			<div class="row">
-				<!--Buttons-->
-				<div class="col-sm-2">
-					<a href="{{route('posts.edit', $post->id)}}" class='btn btn-primary btn-block'>Edit Post</a>
-				</div>
-				<div class="col-sm-2 col-sm-offset-2">
-					<form action="{{route('posts.destroy', $post->id)}}" method='post'>
-						{{method_field("DELETE")}}{{csrf_field()}}
-						<input type="submit" name='delete' value='Delete Post' class='btn btn-danger btn-block'>
-					</form>
-				</div>
-			</div>	
-		@endif
-	</div>	
-
+			@if(Auth::user() && Auth::user()->isAdmin())
+				<hr>
+				<div class="row">
+					<!--Buttons-->
+					<div class="col-sm-2">
+						<a href="{{route('posts.edit', $post->id)}}" class='btn btn-primary btn-block'>Edit Post</a>
+					</div>
+					<div class="col-sm-2 col-sm-offset-2">
+						<form action="{{route('posts.destroy', $post->id)}}" method='post'>
+							{{method_field("DELETE")}}{{csrf_field()}}
+							<input type="submit" name='delete' value='Delete Post' class='btn btn-danger btn-block'>
+						</form>
+					</div>
+				</div>	
+			@endif
+		</div>	
+	</div>
 
 @stop
